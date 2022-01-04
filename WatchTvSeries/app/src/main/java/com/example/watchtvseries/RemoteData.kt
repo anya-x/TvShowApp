33d425fc1dec312553f6206d4e77c04d7c46a69f
Chialing Bragagnolo <chialing.bragagnolo.etu@univ-lille.fr>
2022-01-04 21:34:55 +0100

    add RemoteDataSource class

--- WatchTvSeries/app/src/main/java/com/example/watchtvseries/RemoteData.kt
+++ WatchTvSeries/app/src/main/java/com/example/watchtvseries/RemoteData.kt
@@ -1,4 +1,13 @@
 package com.example.watchtvseries
 
-class RemoteData {
+import com.example.watchtvseries.Model.TvShowJsonData
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val tvShowAPI: TvShowAPI
+) {
+    // TvShow wrapped in Retrofit.Response
+    suspend fun getTvShow(queries: Map<String, String>) {
+        tvShowAPI.getTvShow(queries) }
 }
