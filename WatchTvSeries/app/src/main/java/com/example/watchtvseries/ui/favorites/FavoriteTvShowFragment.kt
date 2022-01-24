cc1043ba737502ff79ab6d18f24477f306f58203
anya-x <ras.lavanya@gmail.com>
2022-01-24 20:21:04 +0100

    small changes

--- WatchTvSeries/app/src/main/java/com/example/watchtvseries/ui/favorites/FavoriteTvShowFragment.kt
+++ WatchTvSeries/app/src/main/java/com/example/watchtvseries/ui/favorites/FavoriteTvShowFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.watchtvseries.R
 import com.example.watchtvseries.data.model.FavoriteTvShow
 import com.example.watchtvseries.data.model.TVShow
@@ -24,6 +25,8 @@ class FavoriteTvShowFragment : Fragment(R.layout.fragment_fav_tv_shows) {
         super.onViewCreated(view, savedInstanceState)
         val favTvShowAdapter = FavoriteTvShowAdapter()
         val binding = FragmentFavTvShowsBinding.bind(view)
+
+        fav_tvShows_recyclerView.layoutManager = GridLayoutManager(activity,2)
 
         favoriteTvShowViewModel.favTvShows.observe(viewLifecycleOwner){
             favTvShowAdapter.setTvShowList(it)
